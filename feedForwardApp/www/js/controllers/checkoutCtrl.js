4cb2c2074c9e699e361f5d5b78319b743e378097
Ellen Blaine <eblaine@stanford.edu>
2017-04-19 10:01:14 -0700

    added checkout form and removed change site label. minial styling and bad dropdowns.

--- feedForwardApp/www/js/controllers/checkoutCtrl.js
+++ feedForwardApp/www/js/controllers/checkoutCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('controllers.checkoutCtrl', [])
-  .controller('CheckoutCtrl', ['checkoutService', function($scope, checkoutService) {
+  .controller('CheckoutCtrl', ['$scope', 'siteService', function($scope, siteService) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -9,6 +9,12 @@ angular.module('controllers.checkoutCtrl', [])
     //
     //$scope.$on('$ionicView.enter', function(e) {
     //});
+    $scope.selectedSite = siteService.getSelectedSite();
+    $scope.sites = siteService.getSites();
+    $scope.updateSite = function(newSite) {
+      $scope.selectedSite = newSite;
+      siteService.setSite(newSite);
+    }
 
-//    $location
+    // TODO add code to send responses to back end
   }]);
