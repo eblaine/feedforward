5c48d2229be9c67128e16dada9a8e5d0414a9e17
Anirudh Jain <anirudh1097@gmail.com>
2017-05-25 13:25:51 -0700

    Pulling questions from Firebase and added rating

--- feedForwardApp/www/js/controllers/checkoutCtrl.js
+++ feedForwardApp/www/js/controllers/checkoutCtrl.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('controllers.checkoutCtrl', [])
-  .controller('CheckoutCtrl', ['$scope', 'siteService', function($scope, siteService) {
+  .controller('CheckoutCtrl', ['$scope', '$location', 'siteService', 'nutritionService', '$firebaseObject', function($scope, $location,  siteService, nutritionService, $firebaseObject) {
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
     // To listen for when this page is active (for example, to refresh data),
@@ -10,6 +10,16 @@ angular.module('controllers.checkoutCtrl', [])
     //$scope.$on('$ionicView.enter', function(e) {
     //});
     $scope.selectedSite = siteService.getSelectedSite();
+    if(!$scope.selectedSite) {
+        return $location.path('/sites');
+    }
+    $scope.foodInfo = nutritionService.getFoodInfo($scope.selectedSite.currFood);
+    var feedbackRef = firebase.database().ref('feedback').child($scope.selectedSite['feedbackObj']);
+    self.feedbackInfo = $firebaseObject(feedbackRef);
+    $scope.checkout = {};			
+    $scope.sendCheckoutForm = function(){
+       console.log(typeof($scope.checkout.rating));
+    };
     // $scope.sites = siteService.getSites();
     // $scope.updateSite = function(newSite) {
     //   $scope.selectedSite = newSite;
