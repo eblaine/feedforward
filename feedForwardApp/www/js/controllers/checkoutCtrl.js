e69472c441f2bffb17f37993b778f037f07e7f82
Anirudh Jain <anirudh1097@gmail.com>
2017-06-07 02:22:45 -0700

    Reading and writing to firebase now working

--- feedForwardApp/www/js/controllers/checkoutCtrl.js
+++ feedForwardApp/www/js/controllers/checkoutCtrl.js
@@ -9,16 +9,87 @@ angular.module('controllers.checkoutCtrl', [])
     //
     //$scope.$on('$ionicView.enter', function(e) {
     //});
+/*
     $scope.selectedSite = siteService.getSelectedSite();
     if(!$scope.selectedSite) {
         return $location.path('/sites');
     }
     // $scope.foodInfo = nutritionService.getFoodInfo($scope.selectedSite.currFood);
+    var questionArray = $firebaseObject(firebase.database().ref('feedback').child($scope.selectedSite.$id));
+    var feedbackQuestionArray = $firebaseObject(firebase.database().ref('feedbackQuestions'));
+    $scope.bonusQuestionID = [];
+    $scope.bonusQuestions = [];
+    questionArray.$loaded().then(function(data){
+           var questionArray = data['questions'];
+           for(var questions in questionArray){
+                if(questionArray[questions]['active']){
+                     $scope.bonusQuestionID.push(questionArray[questions]['questionID']);
+} 
+           }
+            
+feedbackQuestionArray.$loaded().then(function(feedback){
+    for(var question in $scope.bonusQuestionID){
+          $scope.bonusQuestions.push(feedback[$scope.bonusQuestionID[question]]['question']);
+     }
+});
+     });
+*/
 
-    // var feedbackRef = firebase.database().ref('feedback').child($scope.selectedSite.$id);
+
+    $scope.selectedSite = siteService.getSelectedSite();
+    if(!$scope.selectedSite) {
+        return $location.path('/sites');
+    }
+    // $scope.foodInfo = nutritionService.getFoodInfo($scope.selectedSite.currFood);
+    var questionArray = $firebaseObject(firebase.database().ref('feedback').child($scope.selectedSite.$id));
+    var feedbackQuestionArray = $firebaseObject(firebase.database().ref('feedbackQuestions'));
+    $scope.bonusQuestionID = [];
+    $scope.bonusQuestions = [];
+    questionArray.$loaded().then(function(data){
+           var questionArray = data['questions'];
+           for(var questions in questionArray){
+                if(questionArray[questions]['active']){
+                     $scope.bonusQuestionID.push({questionID: questionArray[questions]['questionID'], questionKey: questions});
+} 
+           }
+            
+feedbackQuestionArray.$loaded().then(function(feedback){
+    for(var question in $scope.bonusQuestionID){
+          $scope.bonusQuestions.push({question: feedback[$scope.bonusQuestionID[question].questionID]['question'], answer: -1, id: $scope.bonusQuestionID[question].questionKey});
+     }
+});
+     });
+
+    //console.log(questionArray['questions']);
+    //for(var question in questionArray){
+          //console.log(question);
+   // }
     // var feedbackInfo = $firebaseObject(feedbackRef);
     // $scope.checkout = {};			
 
+$scope.checkout = {};
+$scope.sendCheckoutForm = function(){
+        questionArray['comments'].push($scope.checkout.story);
+        var avgRating = questionArray['avgRating'];
+        var numFeedback = questionArray['totalRatings'];
+        avgRating *= numFeedback;
+        avgRating += $scope.checkout.rating;
+        numFeedback++;
+        questionArray['avgRating'] = avgRating/numFeedback;
+        questionArray['totalRatings'] = numFeedback;
+	for(var questionObj in $scope.bonusQuestions){
+            var question = $scope.bonusQuestions[questionObj];
+            var oldRating = questionArray['questions'][question.id]['rating'];
+            var totalNum = questionArray['questions'][question.id]['totalRatings'];
+            oldRating *= totalNum;
+            oldRating += question.answer;
+            totalNum++;
+            questionArray['questions'][question.id]['rating'] = oldRating/totalNum;
+            questionArray['questions'][question.id]['totalRatings'] = totalNum;
+        }
+        questionArray.$save();
+        return $location.path('/tab/converse');
+}
     // $scope.sendCheckoutForm = function(){
     //    var key = $scope.selectedSite['currFood'];
     //    var nutritionArray = feedbackInfo['nutrition'];
